c88b65d147a95d30b9fc6e681fb92fbb6642c6a8
Aomori <1064643014@qq.com>
2023-06-30 18:07:07 +0800

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,20 @@ module.exports = {
     extend: {},
     container: {
       center: true,
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'green-bg': '#F4F5F4',
+      'banner': '#96A49B',
+      'foot-1': '#9AADA1',
+      'foot-2': 'rgb(158 175 164 / 64%)',
+      'foot-3': 'rgb(164 182 170 / 26%)',
+      'tree-body': '#6E7B72',
+      'tree-root': '#657A6C',
+      'menu': '#727272',
+      'white': '#FFFFFF'
+
     }
   },
   plugins: [],
